350eeb6409f96b9b52f5beff58cbfe73c9102723
Badr <banmark100@gmail.com>
2025-07-08 02:25:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,9 +17,12 @@ bot = commands.Bot(command_prefix="-", intents=intents)
 async def on_ready():
     print(f"✅ Bot is ready! Logged in as {bot.user.name}")
 
-    # Load all Cogs from the cogs folder
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
-            await bot.load_extension(f"cogs.{filename[:-3]}")
+            try:
+                await bot.load_extension(f"cogs.{filename[:-3]}")
+                print(f"✅ Loaded extension: {filename}")
+            except Exception as e:
+                print(f"❌ Failed to load extension {filename}: {e}")
 
-bot.run(os.getenv("DISCORD_TOKEN"))
+bot.run(os.getenv("Your bot token here"))
