8f132ae31e42a77de0493fc6e894fbccb196d31e
Badr <banmark100@gmail.com>
2026-09-17 11:51:51 +0300

    Update db.py

--- db.py
+++ db.py
@@ -2,9 +2,11 @@ from pymongo import MongoClient, errors
 import os
 
 uri = os.getenv("MONGO_URI")
+db_name = os.getenv("DB_NAME", "Prison")
+
 client = MongoClient(uri, tlsAllowInvalidCertificates=True)
 
-db = client["Prison"]
+db = client[db_name]
 collection = db["user"]
 exceptions_collection = db['exceptions']
 guilds_collection = db["guilds"]
@@ -12,7 +14,7 @@ offensive_words_collection = db["offensive_words"]
 settings_collection = db["settings"]
 
 try:
-  client.admin.command('ping')
-  print("You successfully connected to MongoDB!")
+  client.admin.command("ping")
+  print(f"✅ You successfully connected to MongoDB! Database: {db_name}")
 except Exception as e:
   print(e)
